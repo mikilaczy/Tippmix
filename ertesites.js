f7d5c13115f01a610f08ee7c946acebd9930d8e1
KerteszMarton <78353995+KerteszMarton@users.noreply.github.com>
2024-05-23 11:49:26 +0200

    ertesítés

---
+++ ertesites.js
@@ -0,0 +1,9 @@
+const emailForm = document.getElementById('emailForm');
+ 
+emailForm.addEventListener('submit', function(event) {
+  event.preventDefault();
+  const email = document.getElementById('email').value;
+  
+  alert(`Az értesítés elküldve az alábbi e-mail címre: ${email}`);
+ 
+});
