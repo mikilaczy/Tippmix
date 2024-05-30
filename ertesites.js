dcbcf82530564c3ed21e7d7bebd76a07c20841b0
mikilaczy <118450248+mikilaczy@users.noreply.github.com>
2024-05-30 12:00:06 +0200

    Add files via upload

--- ertesites.js
+++ ertesites.js
@@ -3,7 +3,7 @@ const emailForm = document.getElementById('emailForm');
 emailForm.addEventListener('submit', function(event) {
   event.preventDefault();
   const email = document.getElementById('email').value;
-  
+ 
   alert(`Az értesítés elküldve az alábbi e-mail címre: ${email}`);
  
 });
